d50ee63d0bd78e65901bc75f4433ef236ea25c9f
sirparkalotA <dennis.meister@softwareag.com>
2022-05-03 14:24:03 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,9 @@ import { SelectedSpaceComponent } from './selected-space/selected-space.componen
 import { FinalPageComponent } from './final-page/final-page.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    MatSliderModule,
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   declarations: [
     AppComponent,
     HelloComponent,
